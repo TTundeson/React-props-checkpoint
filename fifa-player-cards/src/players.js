8ef9269ec3ea169cef4c8b7d2c4682525b9b7ad0
TTundeson <141639027+TTundeson@users.noreply.github.com>
2023-12-21 15:36:31 +0100

    first commit

---
+++ fifa-player-cards/src/players.js
@@ -0,0 +1,13 @@
+const players = [
+  {
+    name: 'Lionel Messi',
+    team: 'Paris Saint-Germain',
+    nationality: 'Argentina',
+    jerseyNumber: 30,
+    age: 34,
+    imageUrl: 'https://example.com/messi.jpg',
+  },
+  // Add more players as needed
+];
+
+export default players;
